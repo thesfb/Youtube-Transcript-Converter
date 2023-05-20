10b8ee42dc3dfe238b17a3464202630cec0567c7
thesfb <79050959+thesfb@users.noreply.github.com>
2023-05-20 04:03:31 +0000

    Added Dynamic Data

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+app_name = 'Youtube Transcript to PDF converter'
 
 
 @app.route('/')
 def hello():
-  return 'Hello'
+  return render_template('home.html', APP_NAME=app_name)
 
 
 if __name__ == '__main__':
